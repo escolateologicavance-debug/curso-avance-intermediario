6630773aedf2b983230480a5d896ceb12c67b206
escolateologicavance-debug <escolateologicavance@gmail.com>
2025-12-25 23:17:41 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'avance-v1';
+const assets = [
+  './',
+  './index.html',
+  './faq.html',
+  './manifest.json'
+];
+
+// Instalação do Service Worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+// Resposta com Cache
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
